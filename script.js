9af7a32c97f9e072d260fb876f3410befdcae2b1
Manish Chidar <114106502+manish7924@users.noreply.github.com>
2023-10-18 15:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ items.forEach((item, index) => {
     item.addEventListener('click', (e) => { handleIndicator(e.target)});
     item.classList.contains('is-active') && handleIndicator(item);
 });
+
+
+//Get Data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const lessage = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+//Validate Data
+function validateForm(){
+
+    clearMessages();
+
+    if(nameInput.value.length < 1){
+        errorNodes[0].innerText = "Name Cannot Be Blank";
+        nameInput.classList.add("error-border");
+    }
+
+    if(!emailIsValid(email.value)){
+        errorNodes[1].innerText = "Invalid email address";
+        email.Input.classList.add("error-border");
+    }
+}
+//Clear error/ success messages
+function clearMessages(){
+    for(let i=0; i < errorNodes.length; i++){
+        errorNodes[i].innerText = "";
+    }
+    nameInput.classList.remove("error-border");
+}
